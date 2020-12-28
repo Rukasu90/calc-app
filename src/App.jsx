18c0c8d285d6a75e3bcbefc15bff883aacb47c52
RUKASU <lukks666@gmail.com>
2020-12-28 01:23:00 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Result from './components/Result'
+import './App.css'
+
+// Función Flecha o Arrow Function
+const App = () => {
+    // Lo que ejecuta la función
+    console.log("Renderización de App")
+    return (
+    <main className='react-calculator'>
+        <Result value={0} />
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                r
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+    </main>)
+}
+
+export default App
+
---
+++ src/components/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Result = (props) => {
+    debugger
+    console.log("Renderizacion de Result", props)
+    return (
+        <div className="result">
+            <span>0</span>
+        </div>
+    )
+}
+export default Result
