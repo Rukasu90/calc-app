54e3acc2f86b2b2cfd4b619abec7ec318afa41bc
RUKASU <lukks666@gmail.com>
2021-01-18 17:59:24 -0300

    Primer Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,37 @@
 /* eslint no-eval: 0 */
 import React, { useState } from 'react'
+import words from 'lodash.words'
 import Functions from './components/Functions'
 import Numbers from './components/Numbers'
 import MathOperations from './components/MathOperations'
 import Result from './components/Result'
 import './App.css'
 
-// Función Flecha o Arrow Function
+
 const App = () => {
-    //Array Destructuring
     const [stack, setStack] = useState("")
 
-
+    const items = words(stack, /[^-^+^*^/]+/g)
     
-    // Lo que ejecuta la función
-    console.log("Renderización de App")
+    const value = items.length > 0 ? items[items.length-1] : "0";
+    
     return (
     <main className='react-calculator'>
-        <Result value={stack} />
-        <Numbers onClickNumber={number => {
-            console.log("Click en number", number)
-            setStack(`${stack}${number}`)
-        }} />
+        <Result value={value} />
+        <Numbers onClickNumber={number => setStack(`${stack}${number}`)} />
         <Functions 
-            onContentClear={() =>{
-                console.log("Content Clear")
-                setStack('')
-            }}
+            onContentClear={() => setStack('')}
             onDelete={() =>{
                 if (stack.length > 0){
                     const newStack = stack.substring(0, stack.length - 1)
-                    console.log("onDelete", newStack)
                     setStack(newStack)
                 }          
             }}
         />        
         <MathOperations 
-        onClickOperation={operation =>{
-         console.log("Operation:", operation)
-         setStack(`${stack}${operation}`)
-         }}
-         onClickEqual={equal =>{
-            console.log("Equal:", equal)
-            setStack(eval(stack).toString())
-         }} />
+            onClickOperation={operation => setStack(`${stack}${operation}`)}
+            onClickEqual={equal => setStack(eval(stack).toString())}
+         />
     </main>)
 }
 
--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
 
 const renderButtons = onClickNumber => {
-    //var number = 0
-    //iterar desde el boton 1 al 0 (while, for, foreach)
-    //var ArrayComponentes += 
-    //<Button text={number.String()} clickHandler={onClickNumber} />
+    
     const renderButton = number => (
         <Button 
         key={number} 
